8a163b6d3cf9bfd80ecfad9c4976124222473584
wdecoster <decosterwouter@gmail.com>
2020-08-19 13:13:51 +0200

    example from docs, functional

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+// based on https://docs.rs/bio/0.32.0/bio/io/fastq/index.html#read-and-write
+
+use std::io;
+use bio::io::fastq;
+use bio::io::fastq::FastqRead;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut reader = fastq::Reader::new(io::stdin());
+    let mut writer = fastq::Writer::new(io::stdout());
+    let mut record = fastq::Record::new();
+
+    while let Ok(()) = reader.read(&mut record) {
+        if record.is_empty() {
+            let check = record.check();
+            break;
+        }
+
+        let mut sum_qual = record.qual().iter().sum::<u8>() as f64;
+
+        if (sum_qual / record.seq().len() as f64 - 33.0) > 30.0 {
+            writer.write_record(&record);
+        }
+    }
+
 }
