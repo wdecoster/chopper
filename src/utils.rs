edd0999a73a2178665165465f0a24dce48bdea33
wdecoster <decosterwouter@gmail.com>
2025-02-14 09:35:41 +0100

    use std::io::IsTerminal instead of atty

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,8 @@ use std::{
     io::{self, prelude::*, BufRead, BufReader},
     path::Path,
 };
+use std::io::IsTerminal;
+
 
 const MAGIC_MAX_LEN: usize = 6;
 const GZ_MAGIC: [u8; 3] = [0x1f, 0x8b, 0x08];
@@ -85,7 +87,7 @@ where
             Ok(Box::new(BufReader::with_capacity(BUFF_SIZE, fp)))
         }
     } else {
-        if atty::is(atty::Stream::Stdin) {
+        if io::stdin().is_terminal() {
             eprintln!("Error: stdin not detected");
             std::process::exit(1);
         }
